b7deb4bd108257d6270e897b5d21688b59cfdef4
vidheshashetty <vidheshashetty@gmail.com>
2026-04-08 13:32:53 +0530

    Update upload_large_orders_to_s3.py

--- scripts/upload_large_orders_to_s3.py
+++ scripts/upload_large_orders_to_s3.py
@@ -1,21 +1,24 @@
 import boto3
 import os
+import argparse
 
-LOCAL_FILE_PATH = "data/data.csv"
-BUCKET_NAME = "ecommerce"
-S3_KEY = "raw/data/orders_2.csv"
-
-def upload_to_s3():
+def upload_to_s3(local_file_path, bucket_name, s3_key):
     s3 = boto3.client('s3')
 
-    if not os.path.exists(LOCAL_FILE_PATH):
-        raise FileNotFoundError(f"{LOCAL_FILE_PATH} not found")
+    if not os.path.exists(local_file_path):
+        raise FileNotFoundError(f"{local_file_path} not found")
 
     print("Uploading file to S3...")
-
-    s3.upload_file(LOCAL_FILE_PATH, BUCKET_NAME, S3_KEY)
-
-    print(f"✅ Uploaded to s3://{BUCKET_NAME}/{S3_KEY}")
+    s3.upload_file(local_file_path, bucket_name, s3_key)
+    print(f"✅ Uploaded to s3://{bucket_name}/{s3_key}")
 
 if __name__ == "__main__":
-    upload_to_s3()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file_path", required=True, help="Local CSV path")
+    args = parser.parse_args()
+
+    upload_to_s3(
+        local_file_path=args.file_path,
+        bucket_name="ecommerce",
+        s3_key="raw/orders_5m/orders_5m.csv"
+    )
