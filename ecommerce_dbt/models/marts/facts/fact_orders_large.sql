7e56a03d54287f62350dd270d5ffcf9544094287
vidheshashetty <vidheshashetty@gmail.com>
2026-04-08 13:11:52 +0530

    Create fact_orders_large.sql

---
+++ ecommerce_dbt/models/marts/facts/fact_orders_large.sql
@@ -0,0 +1,33 @@
+{{ config(materialized='table') }}
+
+WITH source AS (
+
+    SELECT
+        order_id,
+        customer_id,
+        product_id,
+        amount,
+        order_date
+    FROM {{ source('raw', 'orders_large') }}
+
+),
+
+final AS (
+
+    SELECT
+        o.order_id,
+        o.order_date,
+        o.customer_id,
+        c.customer_name,
+        o.product_id,
+        p.product_name,
+        o.amount
+    FROM source o
+    LEFT JOIN {{ ref('dim_customers') }} c
+        ON o.customer_id = c.customer_id
+    LEFT JOIN {{ ref('dim_products') }} p
+        ON o.product_id = p.product_id
+
+)
+
+SELECT * FROM final
